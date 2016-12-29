0c9421e1f82ee7d4b6430326b87e89960e353e0f
MagicLegend <aapje112513@hotmail.nl>
2016-12-29 22:49:16 +0100

    v0.1 commit

---
+++ TW-Hotkey-Fixer.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name         TW Hotkey-fixer
+// @version      0.1
+// @description  Fixes the behaviour of the TW hotkeys when on the recruiting pages
+// @author       MagicLegend
+// @include      https://*.tribalwars.*/game.php*
+// ==/UserScript==
+
+/** LICENCE:
+ * TW Hotkey-fixer v0.1 © 2016 MagicLegend
+ * This work is under the Attribution-NonCommercial-ShareAlike 4.0 International (CC BY-NC-SA 4.0) Licence.
+ * More info can be found here: https://creativecommons.org/licenses/by-nc-sa/4.0/deed.en (Human readable, not the actual licence) & https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode (Actual licence)
+ *
+ * v0.1		Initial work, first working version
+ */
+
+if (location.href.match(/(nl|zz|en).*\.tribalwars\.(nl|net)\/game\.php(\?|.*\&)screen\=train(\?|.*\&)mode\=train/)) {
+    $(".vis").keydown(function (e) {
+        console.log("Handler for .keydown() called.");
+        if (e.which == 65) {
+            console.log("a");
+            var direction = $("#village_switch_right").attr("href");
+            document.location.replace(direction);
+        } else if (e.which == 68) {
+            console.log("d");
+            var direction = $("#village_switch_left").attr("href");
+            document.location.replace(direction);
+        };
+    });
+};
